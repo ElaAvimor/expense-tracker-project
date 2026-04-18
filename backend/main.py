8e2fc543db6d81feb7aeea282eaf68c95e6e514a
Ela Avimor <eavimo12@gmail.com>
2026-04-18 20:06:46 +0300

    create frontend server - firebase

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,13 @@ from db import models
 
 app = FastAPI() # creates the app
 
-# since frontend and backend run on different ports (origins), CORS middleware is needed to allow the frontend to make requests to the backend
-# CORS = cross origin resource sharing
+# since frontend and backend run on different ports (origins),
+# CORS middleware is needed to allow the frontend to make requests to the backend
+# CORS = cross-origin resource sharing
 allow_origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://YOUR_FRONTEND_DOMAIN",
+    "https://expense-tracker-3dc4c.web.app",
 ]
 
 app.add_middleware(
--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 const API_BASE =
   window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"
-    : "https://your-backend-url";
+    : "https://expense-backend-1065487092168.us-central1.run.app/";
 
 // ── State ────────────────────────────────────────────────
 
